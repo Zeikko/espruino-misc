66e5b488dd42fb82655b0996c5eb85e9989dc478
Jaakko Ojalehto <jaakko@ojalehto.fi>
2014-02-10 22:52:43 +0200

    Added lightwave.

---
+++ lightWave.js
@@ -0,0 +1,65 @@
+/**
+ * Pulse wave modulated light wave
+ * Blinks red, green and blue LEDs each in a turn.
+ * @FIXME Keeps throwing Unkown Interval ERRORS. Can't figure out why. Can you?
+ */
+var intervals = [];
+/**
+ * Pulse Wave Modulation
+ * Blinks led faster than the eye can see to adjust brightness of a light
+ * Brightness is a float from 0 to 1
+ * Recommended frequency over 50
+ * 
+ */
+function pwm(led, brightness, frequency) {
+  if ((typeof intervals[led]) !== "undefined") {
+    clearInterval(intervals[led]);
+    led.reset();
+  }
+  if(brightness > 0) {
+    intervals[led] = setInterval(function() {
+      digitalPulse(led, 1, brightness * (1000/frequency));
+    }, 1000/frequency);
+  }
+}
+
+var interval;
+/**
+* Changes brightness between 0.1 to 1 with 0.1 intervals with determined interval.
+*/
+function wave(led, time) {
+  var brightness = 0.1,
+      change = 0.1;
+  interval = setInterval(function() {
+    brightness = brightness + change;
+    if(brightness > 0.9 || brightness < 0.2) {
+      change *= -1;
+    }
+    pwm(led, brightness, 500);
+
+
+  }, time);
+}
+
+var activeLed;
+/**
+* Starts the waving a LED, waits while a single wave is over. Turns of the led and calls itself recursively activating second LED.
+*/
+function blink() {
+  if((typeof interval) !== "undefined") {
+    clearInterval(interval);
+  }
+  pwm(activeLed, 0, 500);
+  if(activeLed == LED1) {
+    activeLed = LED2;
+  } else if(activeLed == LED2) {
+    activeLed = LED3;
+  } else {
+    activeLed = LED1;
+  }
+  wave(activeLed, 50);
+  setTimeout(blink, 800);
+ }
+
+blink();
+
